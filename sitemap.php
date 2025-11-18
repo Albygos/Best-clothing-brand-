d41676fd4915e4dc6637d9434b0e4001f17d2924
Albygos <163895834+Albygos@users.noreply.github.com>
2025-11-18 14:49:24 +0530

    Create sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,16 @@
+<?php
+$dir = "pages/";
+$files = scandir($dir);
+$sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+$sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+foreach ($files as $file) {
+    if ($file == '.' || $file == '..') continue;
+    $url = "https://luxeloom.in/pages/" . urlencode($file);
+    $sitemap .= "<url><loc>$url</loc><priority>0.80</priority></url>";
+}
+
+$sitemap .= "</urlset>";
+file_put_contents("sitemap.xml", $sitemap);
+echo "Sitemap Created Successfully!";
+?>
