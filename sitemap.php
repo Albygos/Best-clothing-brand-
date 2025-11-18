693fb2241316770baffeb887cc63c854d9c1b019
Albygos <163895834+Albygos@users.noreply.github.com>
2025-11-18 15:17:39 +0530

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -6,7 +6,7 @@ $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 foreach ($files as $file) {
     if ($file == '.' || $file == '..') continue;
-    $url = "https://luxeloom.in/pages/" . urlencode($file);
+    $url = "https://best-clothing-brand.onrender.com" . urlencode($file);
     $sitemap .= "<url><loc>$url</loc><priority>0.80</priority></url>";
 }
 
